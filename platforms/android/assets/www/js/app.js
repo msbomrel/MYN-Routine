077b0e5d62e06cd197810c9c5a3ae9fbd74087a4
msbomrel <mohan.bomrel@deerwalk.edu.np>
2016-04-04 19:14:43 +0545

    changes in about us and

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,109 +1,103 @@
-var app=angular
+var app = angular
 
-  .module('routineApp',['ionic','ngStorage','ui.router','myFactory','myController','ngMaterial'])
+    .module('routineApp', ['ionic', 'ngStorage', 'ui.router', 'myFactory', 'myController', 'ngMaterial'])
 
-  .run(function($rootScope, $ionicPlatform){
-  $ionicPlatform.registerBackButtonAction(function(e){
-    if ($rootScope.backButtonPressedOnceToExit) {
-      ionic.Platform.exitApp();
-    }
-    else {
-      $rootScope.backButtonPressedOnceToExit = true;
-      ionic.Platform.exitApp();
-      setTimeout(function(){
-        $rootScope.backButtonPressedOnceToExit = false;
-      },100);
-    }
-    e.preventDefault();
-    return false;
-  },101);
+    .run(function ($rootScope, $ionicPlatform) {
+        $ionicPlatform.registerBackButtonAction(function (e) {
+            if ($rootScope.backButtonPressedOnceToExit) {
+                ionic.Platform.exitApp();
+            }
+            else {
+                $rootScope.backButtonPressedOnceToExit = true;
+                ionic.Platform.exitApp();
+                setTimeout(function () {
+                    $rootScope.backButtonPressedOnceToExit = false;
+                }, 100);
+            }
+            e.preventDefault();
+            return false;
+        }, 101);
 
-})
+    })
 
-  .config(function($stateProvider, $urlRouterProvider){
-      $urlRouterProvider.otherwise('/');
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
 
-      $stateProvider
-      .state('day1', {
-        url: "/day1",
-        templateUrl: "templates/monday.html"
-      })
-      .state('day2', {
-        url: "/day2",
-        templateUrl: "templates/tuesday.html"
-      })
-      .state('day3', {
-        url: "/day3",
-        templateUrl: "templates/wednesday.html"
-      })
-      .state('day4', {
-        url: "/day4",
-        templateUrl: "templates/thursday.html"
-      })
-      .state('day5', {
-        url: "/day5",
-        templateUrl: "templates/friday.html"
-      })
-    ;
-  })
+        $stateProvider
+            .state('day1', {
+                url: "/day1",
+                templateUrl: "templates/monday.html"
+            })
+            .state('day2', {
+                url: "/day2",
+                templateUrl: "templates/tuesday.html"
+            })
+            .state('day3', {
+                url: "/day3",
+                templateUrl: "templates/wednesday.html"
+            })
+            .state('day4', {
+                url: "/day4",
+                templateUrl: "templates/thursday.html"
+            })
+            .state('day5', {
+                url: "/day5",
+                templateUrl: "templates/friday.html"
+            })
+        ;
+    })
 
-  .controller('tabCtrl', function($scope, $location) {
-      var d= new Date();
-      var n= d.getDay();
-    $scope.selectedIndex = n - 1;
+    .controller('tabCtrl', function ($scope, $location) {
+        var d = new Date();
+        var n = d.getDay();
+        $scope.selectedIndex = n - 1;
 
-    $scope.onSwipeRight = function (){
-      if ($scope.selectedIndex < 4){
-        $scope.selectedIndex  = $scope.selectedIndex + 1;
-      }
-      // if you want to make all the tour
-      else{
-        $scope.selectedIndex  = 0;
-      }
-    }
+        $scope.onSwipeRight = function () {
+            if ($scope.selectedIndex < 4) {
+                $scope.selectedIndex = $scope.selectedIndex + 1;
+            }
+            // if you want to make all the tour
+            else {
+                $scope.selectedIndex = 0;
+            }
+        }
 
-    $scope.onSwipeLeft = function () {
+        $scope.onSwipeLeft = function () {
 
-      if ($scope.selectedIndex > 0){
-        $scope.selectedIndex  = $scope.selectedIndex - 1;
-      }
-      // if you want to make all the tour
-      else {
-        $scope.selectedIndex  = 4;
-      }
-    }
-    $scope.$watch('selectedIndex', function(current) {
-      switch (current) {
-        case 0:
-          $location.url("/day1");
-          break;
-        case 1:
-          $location.url("/day2");
-          break;
-        case 2:
-          $location.url("/day3");
-          break;
-        case 3:
-          $location.url("/day4");
-          break;
-        case 4:
-          $location.url("/day5");
-          break;
-      }
-    });
-  })
+            if ($scope.selectedIndex > 0) {
+                $scope.selectedIndex = $scope.selectedIndex - 1;
+            }
+            // if you want to make all the tour
+            else {
+                $scope.selectedIndex = 4;
+            }
+        }
+        $scope.$watch('selectedIndex', function (current) {
+            switch (current) {
+                case 0:
+                    $location.url("/day1");
+                    break;
+                case 1:
+                    $location.url("/day2");
+                    break;
+                case 2:
+                    $location.url("/day3");
+                    break;
+                case 3:
+                    $location.url("/day4");
+                    break;
+                case 4:
+                    $location.url("/day5");
+                    break;
+            }
+        });
+    })
 
-  .controller('sideController', function($scope, $mdSidenav,$log) {
-  $scope.openRightMenu = function() {
-      var d= new Date();
-      var n= d.getDay();
-    $mdSidenav('right').toggle()
-      .then(function () {
-        $log.debug("Toggle is trigerred");
-      })
-      .then(function () {
-          $log.debug(n);
-      });
-  };
-})
+    .controller('sideController', function ($scope, $mdSidenav) {
+        $scope.openRightMenu = function () {
+            $mdSidenav('right').toggle();
+        };
+    })
+
+   
 ;
--- platforms/android/assets/www/js/controller.js
+++ platforms/android/assets/www/js/controller.js
@@ -1,142 +1,163 @@
 /**
  * Created by root on 2/26/16.
  */
-var app=angular
-    .module('myController',[])
+var app = angular
+    .module('myController', [])
 
 
     .controller('GlobalController',
-        function ($scope,$location,$localStorage,$log,$window,$state,
-                  Class19A,Class19B,Class18A,Class18B,Class17A,Class17B,Class16A,Class16B) {
-        $scope.selectChanged = function(){
-            if ($scope.someVal == 1){
-                $scope.update();
-                $scope.sixteenA();
-                $window.location.reload();
-            }
-            else if($scope.someVal == 2){
-                $scope.update();
-                $scope.sixteenB();
-                $window.location.reload();
+        function ($scope, $location, $localStorage, $log, $window, $state, $mdDialog,
+                  Class19A, Class19B, Class18A, Class18B, Class17A, Class17B, Class16A, Class16B) {
 
-            }
-            else if($scope.someVal == 3){
-                $scope.update();
-                $scope.seventeenA();
-                $window.location.reload();
+            $scope.showDialog = function () {
+                var alert;
+                alert = $mdDialog.alert({
+                    content: 'Developers: Mohan, Yasin  Designer: Nikita',
+                    ok: 'Okay'
+                });
 
-            }
-            else if($scope.someVal == 4){
-                $scope.update();
-                $scope.seventeenB();
-                $window.location.reload();
+                $mdDialog
+                    .show(alert)
+                    .finally(function () {
+                        alert = undefined;
+                    });
+            };
 
-            }
-            else if($scope.someVal == 5){
-                $scope.update();
-                $scope.eighteenA();
-                $window.location.reload();
-            }
-            else if($scope.someVal == 6){
-                $scope.update();
-                $scope.eighteenB();
-                $window.location.reload();
-            }
-            else if($scope.someVal == 7){
-                $scope.update();
-                $scope.nineteenA();
-                $window.location.reload();
-            }
-            else {
-                $scope.update();
-                $scope.nineteenB();
-                $window.location.reload();
-            }
-        };
+            $scope.selectChanged = function () {
+                if ($scope.someVal == 1) {
+                    $scope.update();
+                    $scope.sixteenA();
 
-        /* $scope.goToWhere= function () {
-         $location.path('/day'+n);
-         };*/
+                    $window.location.reload();
 
-        $scope.nineteenA = function () {
-            Class19A.query(function (data) {
-                $localStorage.store_nineteenA = data;
-            });
+                }
+                else if ($scope.someVal == 2) {
+                    $scope.update();
+                    $scope.sixteenB();
+                    $window.location.reload();
 
-            $localStorage.sem = ' 19 A';
-            $localStorage.message = $localStorage.store_nineteenA;
-           /* $location.path('/day' + n);*/
-        };
-        $scope.nineteenB = function () {
-            Class19B.query(function (data) {
-                $localStorage.store_nineteenB = data;
-            });
-            $localStorage.sem = ' 19 B';
-            $localStorage.message = $localStorage.store_nineteenB;
-            /*$location.path('/day' + n);*/
-        };
-        $scope.eighteenA = function () {
-            Class18A.query(function (data) {
-                $localStorage.store_eighteenA = data;
-            });
-            $localStorage.sem = ' 18 A';
-            $localStorage.message = $localStorage.store_eighteenA;
-            /*$location.path('/day' + n);*/
-        };
-        $scope.eighteenB = function () {
-            Class18B.query(function (data) {
-                $localStorage.store_eighteenB = data;
-            });
-            $localStorage.sem = ' 18 B';
-            $localStorage.message = $localStorage.store_eighteenB;
-            /*$location.path('/day' + n);*/
-        };
-        $scope.seventeenA = function () {
-            Class17A.query(function (data) {
-                $localStorage.store_seventeenA = data;
-            });
-            $localStorage.sem = ' 17 A';
-            $localStorage.message = $localStorage.store_seventeenA;
-            /*$location.path('/day'+n);*/
-        };
-        $scope.seventeenB = function () {
-            Class17B.query(function (data) {
-                $localStorage.store_seventeenB = data;
-            });
-            $localStorage.sem = ' 17 B';
-            $localStorage.message = $localStorage.store_seventeenB;
-            /*$location.path('/day'+n);*/
-        };
-        $scope.sixteenA = function () {
-            Class16A.query(function (data) {
-                $localStorage.store_sixteenA = data;
-            });
-            $localStorage.sem = ' 16 A';
-            $localStorage.message = $localStorage.store_sixteenA;
-            //$location.path('/day' + n);
-        };
-        $scope.sixteenB = function () {
-            Class16B.query(function (data) {
-                $localStorage.store_sixteenB = data;
-            });
-            $localStorage.sem = ' 16 B';
-            $localStorage.message = $localStorage.store_sixteenB;
-            //$location.path('/day' + n);
-        };
-        $scope.getInReal=function(){
+                }
+                else if ($scope.someVal == 3) {
+                    $scope.update();
+                    $scope.seventeenA();
+                    $window.location.reload();
+
+                }
+                else if ($scope.someVal == 4) {
+                    $scope.update();
+                    $scope.seventeenB();
+                    $window.location.reload();
+
+                }
+                else if ($scope.someVal == 5) {
+                    $scope.update();
+                    $scope.eighteenA();
+                    $window.location.reload();
+                }
+                else if ($scope.someVal == 6) {
+                    $scope.update();
+                    $scope.eighteenB();
+                    $window.location.reload();
+                }
+                else if ($scope.someVal == 7) {
+                    $scope.update();
+                    $scope.nineteenA();
+                    $window.location.reload();
+                }
+                else {
+                    $scope.update();
+                    $scope.nineteenB();
+                    $window.location.reload();
+                }
+            };
+
+            /* $scope.goToWhere= function () {
+             $location.path('/day'+n);
+             };*/
+
+            $scope.nineteenA = function () {
+                Class19A.query(function (data) {
+                    $localStorage.store_nineteenA = data;
+                });
+
+                $localStorage.sem = ' 19 A';
+                $localStorage.message = $localStorage.store_nineteenA;
+                /* $location.path('/day' + n);*/
+            };
+            $scope.nineteenB = function () {
+                Class19B.query(function (data) {
+                    $localStorage.store_nineteenB = data;
+                });
+                $localStorage.sem = ' 19 B';
+                $localStorage.message = $localStorage.store_nineteenB;
+                /*$location.path('/day' + n);*/
+            };
+            $scope.eighteenA = function () {
+                Class18A.query(function (data) {
+                    $localStorage.store_eighteenA = data;
+                });
+                $localStorage.sem = ' 18 A';
+                $localStorage.message = $localStorage.store_eighteenA;
+                /*$location.path('/day' + n);*/
+            };
+            $scope.eighteenB = function () {
+                Class18B.query(function (data) {
+                    $localStorage.store_eighteenB = data;
+                });
+                $localStorage.sem = ' 18 B';
+                $localStorage.message = $localStorage.store_eighteenB;
+                /*$location.path('/day' + n);*/
+            };
+            $scope.seventeenA = function () {
+                Class17A.query(function (data) {
+                    $localStorage.store_seventeenA = data;
+                });
+                $localStorage.sem = ' 17 A';
+                $localStorage.message = $localStorage.store_seventeenA;
+                /*$location.path('/day'+n);*/
+            };
+            $scope.seventeenB = function () {
+                Class17B.query(function (data) {
+                    $localStorage.store_seventeenB = data;
+                });
+                $localStorage.sem = ' 17 B';
+                $localStorage.message = $localStorage.store_seventeenB;
+                /*$location.path('/day'+n);*/
+            };
+            $scope.sixteenA = function () {
+                Class16A.query(function (data) {
+                    $localStorage.store_sixteenA = data;
+                });
+                $localStorage.sem = ' 16 A';
+                $localStorage.message = $localStorage.store_sixteenA;
+                //$location.path('/day' + n);
+            };
+            $scope.sixteenB = function () {
+                Class16B.query(function (data) {
+                    $localStorage.store_sixteenB = data;
+                });
+                $localStorage.sem = ' 16 B';
+                $localStorage.message = $localStorage.store_sixteenB;
+                //$location.path('/day' + n);
+            };
+            $scope.getInReal = function () {
+                $scope.subjects = $localStorage.message;
+                $scope.semester = $localStorage.sem;
+            };
+
             $scope.subjects = $localStorage.message;
             $scope.semester = $localStorage.sem;
-        };
 
-        $scope.subjects = $localStorage.message;
-        $scope.semester = $localStorage.sem;
-
-        $scope.update = function () {
-            $scope.sixteenA();$scope.sixteenB();
-            $scope.seventeenA();$scope.seventeenB();
-            $scope.eighteenA();$scope.eighteenB();
-            $scope.nineteenA();$scope.nineteenB();
-            $scope.getInReal();
-        }
-    })
+            $scope.update = function () {
+                $scope.sixteenA();
+                $scope.sixteenB();
+                $scope.seventeenA();
+                $scope.seventeenB();
+                $scope.eighteenA();
+                $scope.eighteenB();
+                $scope.nineteenA();
+                $scope.nineteenB();
+                $scope.getInReal();
+            }
+        })
     ;
